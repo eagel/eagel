fc78b3abe1aa7fb9115b6d26ffdf371aeb8a2ad6
eagel <angelfalcon@qq.com>
2016-04-05 13:56:52 +0800

    - add class logger.

---
+++ lib/logger.cc
@@ -0,0 +1,5 @@
+#include "logger.hh"
+
+namespace ea {
+
+} /* namespace ea */
---
+++ lib/logger.hh
@@ -0,0 +1,11 @@
+#ifndef EA_LOGGER_HH_
+#define EA_LOGGER_HH_
+
+namespace ea {
+
+class logger {
+};
+
+} /* namespace ea */
+
+#endif /* EA_LOGGER_HH_ */
